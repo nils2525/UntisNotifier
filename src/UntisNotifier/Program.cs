ff23382ddf392d9c085d266150c395fb83a30496
nils2525 <nilslehb@gmail.com>
2019-02-07 08:46:18 +0100

    project init; added WebUntis models

---
+++ src/UntisNotifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UntisNotifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ src/UntisNotifier/WebUntis/Models/LoginResult.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UntisNotifier.WebUntis.Models
+{
+    public class LoginResult
+    {
+        [JsonProperty("reload")]
+        public bool Reload { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+    }
+}
