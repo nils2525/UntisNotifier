9f80bd355509f2a9bfa34eb8efc7e8c00f703b5a
JonKAS <kay.kahmann@gmail.com>
2019-02-08 13:30:24 +0100

    get credentials from the program args

--- src/UntisNotifier/Program.cs
+++ src/UntisNotifier/Program.cs
@@ -5,10 +5,20 @@ namespace UntisNotifier
 {
     class Program
     {
+        //args constructed
+        //args[0] username
+        //args[1] password
+        //args[2] school
+        //args[3] --notifiers
+        //args[3+n] {notifier name}
         static async Task Main(string[] args)
         {
-            var client = new WebUntis.Client("user", "password", "c-severing-bk", "https://mese.webuntis.com/WebUntis");
-            await client.LoginAsync();
+            var client = new WebUntis.Client(args[0], args[1], args[2], "https://mese.webuntis.com/WebUntis");
+            var isLoggedIn = await client.LoginAsync();
+            if (isLoggedIn)
+            {
+                var lessons = await client.GetChangedLessons();
+            }
         }
     }
 }
